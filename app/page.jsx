6f95bb7276bb26a09d2bf9277fcd769b67b8c8bb
SCYTHE6 <divyanshuchauhan1283@gmail.com>
2024-12-20 14:18:46 +0530

    port

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
+import {Typewriter} from 'react-simple-typewriter';
 
 // components
 import Social from "@/components/Social";
@@ -7,18 +10,47 @@ import Photo from "@/components/Photo";
 import Stats from "@/components/Stats";
 
 const Home = () => {
+
+  // Define handleDone and handleType functions
+  const handleDone = () => {
+    console.log("Typing animation completed!");
+  };
+
+  const handleType = (count) => {
+    console.log(`Typing event fired ${count} times`);
+  };
+
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl">Frontend Developer</span>
+          <div className="text-xl">
+            <Typewriter
+            words={['Frontend Developer', 'Software Developer', 'Machine Learning Engineer']}
+            loop={5}
+            cursor
+            cursorStyle='_'
+            typeSpeed={100}
+            deleteSpeed={50}
+            delaySpeed={1000}
+            onLoopDone={handleDone}
+            onType={handleType}
+          />
+          {/* <span >Frontend Developer</span> */}
+          </div>
             <h1 className="h1 mb-6">
               Hello I'm <br /> <span className="text-accent">Divyanshu Chauhan</span>
             </h1>
             <p className="max-w-[500px} mb-9 text-white/80">
-              I excel at crafting elegant & digital websites and I am proficient in various programming languages and technologies.
+            I specialize in designing and developing sophisticated, 
+            visually appealing digital websites and possess extensive expertise 
+            in a diverse range of programming languages and technologies. 
+            Additionally, 
+            I have a strong proficiency in the training and development of 
+            advanced artificial intelligence and machine learning models, 
+            enabling innovative and data-driven solutions.
             </p>
             {/* btn and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
